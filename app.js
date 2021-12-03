914362d9ac2aeeee495c6bfc64e83f32384e0402
Владимир Филимоненко <zrnrd@mail.ru>
2021-12-03 13:17:42 +0500

    Fixed changing of notes

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 /* Открыть-закрыть меню */
-document.querySelector('.menu-show').addEventListener("click", (e)=>{
+document.querySelector('.menu-show').addEventListener("click", ()=>{
     document.querySelector('.menu-items').classList.toggle("menu-items_show")
 })
 
 /* Переключение режимов */
 document.querySelector('.menu-items').addEventListener("click",(e)=>{
+    const timeControlHeader = document.querySelector(".time-control__header");
     const timer = document.querySelector(".timer");
     const stopwatch = document.querySelector(".stopwatch");
 
     if(e.target.value === "Таймер"){
+
+        timeControlHeader.innerText = "Таймер"
         timer.classList.remove("hide")
         stopwatch.classList.add("hide")
+
+        document.querySelectorAll('.menu-item').forEach((item)=>{
+            if(item.value !== e.target.value){
+                item.classList.remove("menu-item_active")
+            }else{
+                item.classList.add("menu-item_active")
+            }
+        })
+
     }else if(e.target.value === "Секундомер"){
+
+        timeControlHeader.innerText = "Секундомер"
         stopwatch.classList.remove("hide")
         timer.classList.add("hide")
+
+        document.querySelectorAll('.menu-item').forEach((item)=>{
+            if(item.value !== e.target.value){
+                item.classList.remove("menu-item_active")
+            }else{
+                item.classList.add("menu-item_active")
+            }
+        })
     }
 })
 
