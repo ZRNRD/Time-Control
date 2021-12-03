2d3615bb39d6a68426194aba11a2c799aa9e8efd
Владимир Филимоненко <zrnrd@mail.ru>
2021-12-03 13:07:43 +0500

    Added changing of modes

--- app.js
+++ app.js
@@ -2,3 +2,21 @@
 document.querySelector('.menu-show').addEventListener("click", (e)=>{
     document.querySelector('.menu-items').classList.toggle("menu-items_show")
 })
+
+/* Переключение режимов */
+document.querySelector('.menu-items').addEventListener("click",(e)=>{
+    const timer = document.querySelector(".timer");
+    const stopwatch = document.querySelector(".stopwatch");
+
+    if(e.target.value === "Таймер"){
+        timer.classList.remove("hide")
+        stopwatch.classList.add("hide")
+    }else if(e.target.value === "Секундомер"){
+        stopwatch.classList.remove("hide")
+        timer.classList.add("hide")
+    }
+})
+
+/* let hours = 0;
+let minutes = 0;
+let seconds = 0; */
