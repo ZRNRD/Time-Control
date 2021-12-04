91997c93d1873146c4084fdfed600cdacd6002e6
Владимир Филимоненко <zrnrd@mail.ru>
2021-12-04 13:24:10 +0500

    Finished Timer

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ let hours = 0;
 let minutes = 0;
 let seconds = 0;
 
+/* Изменение чисел в таймере */
 document.querySelector(".timer__nums").addEventListener("click", (e)=>{
     if(e.target.classList.contains("num__up") && e.target.parentNode.classList.contains("timer__hours")){
         hours--;
@@ -72,6 +73,132 @@ document.querySelector(".timer__nums").addEventListener("click", (e)=>{
     }
 })
 
+/* Запуск, пауза и выключение таймера */
+const timerStart = document.querySelector(".timer__start");
+const timerCancel = document.querySelector(".timer__cancel");
+const timerPause = document.querySelector(".timer__pause");
+
+timerStart.addEventListener("click", (e)=>{
+    e.currentTarget.classList.add("hide")
+
+    timerCancel.classList.remove("hide")
+    timerPause.classList.remove("hide")
+
+    blockButtons([".timer__cancel", ".timer__pause"])
+    startTimer()
+
+})
+
+timerCancel.addEventListener("click", (e)=>{
+    e.currentTarget.classList.add("hide")
+    timerPause.classList.add("hide")
+
+    timerStart.classList.remove("hide")
+    cancelTimer()
+    unblockButtons()
+
+})
+
+timerPause.addEventListener("click", (e)=>{
+    pauseTimer()
+
+    if(e.currentTarget.value === "ПАУЗА"){
+        e.currentTarget.value = "ДАЛЕЕ"
+    }else if(e.currentTarget.value === "ДАЛЕЕ"){
+        e.currentTarget.value = "ПАУЗА"
+        startTimer()
+    }
+
+    unblockButtons()
+})
+
+
 function getCorrectNum(num){
     return num < 10 ? "0" + num : num;
+}
+
+function blockButtons(exeptions){
+    document.querySelectorAll('input[type=button]').forEach((btn)=>{
+        btn.disabled = true;
+    })
+
+    exeptions.forEach((ex)=>{
+        document.querySelector(ex).disabled = false
+    })
+}
+
+function unblockButtons(){
+    document.querySelectorAll('input[type=button]').forEach((btn)=>{
+        btn.disabled = false;
+    })
+}
+
+    
+let interval = null;
+
+function startTimer(){
+    let bell = new Audio("./sound/bell.mp3");
+    if(hours === 0 && minutes === 0 && seconds === 0){
+        bell.play();
+        cancelTimer()
+        return;
+    }
+
+
+    interval = setInterval(()=>{
+        seconds--;
+        if(seconds < 0){
+            seconds = helper(seconds);
+            minutes--;
+        }
+        if(minutes < 0){
+            minutes = helper(minutes);
+            hours--;
+        }
+
+        document.querySelector(".timer__hours").querySelector(".num").innerText = getCorrectNum(hours)
+        document.querySelector(".timer__minutes").querySelector(".num").innerText = getCorrectNum(minutes)
+        document.querySelector(".timer__seconds").querySelector(".num").innerText = getCorrectNum(seconds)
+
+        if(hours === 0 && minutes === 0 && seconds === 0){
+            bell.play();
+            cancelTimer();
+        }
+        
+    },1000)
+}
+
+function helper(num){
+    if(num < 0){
+        return 59;
+    }else if(num > 59){
+        return 0
+    }else{
+        return num
+    }
+}
+
+function cancelTimer(){
+    pauseTimer()
+
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    document.querySelector(".timer__hours").querySelector(".num").innerText = getCorrectNum(hours)
+    document.querySelector(".timer__minutes").querySelector(".num").innerText = getCorrectNum(minutes)
+    document.querySelector(".timer__seconds").querySelector(".num").innerText = getCorrectNum(seconds)
+
+    timerStart.classList.remove("hide")
+    timerCancel.classList.add("hide")
+    timerPause.classList.add("hide")
+
+    
+}
+
+function pauseTimer(){
+    window.clearInterval(interval);
+    interval = null;
+    timerPause.value = "ПАУЗА"
+    unblockButtons()
 }
